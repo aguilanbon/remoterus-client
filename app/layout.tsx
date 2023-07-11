692f53d5ab9368a4f16cdc36fe93017a99c66d3e
Bon Aguilan <aguilanbon@gmail.com>
2023-07-11 16:17:17 +0800

    added shadcn button,form, and zod packages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,19 @@
-import './globals.css'
-import type { Metadata } from 'next'
+import "./globals.css";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'Remote R Us',
-  description: 'Developers searching for a place remotely',
-}
+  title: "Remote R Us",
+  description: "Remoteers new Home",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
