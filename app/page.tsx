2dd81f47888f29be465fdaaabf9d993b8c1d95c5
Bon Aguilan <aguilanbon@gmail.com>
2023-07-11 21:35:36 +0800

    render signin component in signin page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import Image from 'next/image'
+import SignInForm from "@/components/forms/signin_form";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      Init
+    <div className="w-full min-h-screen">
+      <div className="w-full h-screen flex items-center justify-center">
+        <div className="w-96 bg-white p-10 rounded-md">
+          <SignInForm />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
