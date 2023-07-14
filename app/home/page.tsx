93ab109cd415d51c8d88cc410a7ae78ab5ddf342
Bon Aguilan <aguilanbon@gmail.com>
2023-07-14 14:47:28 +0800

    initial home page layout/page

---
+++ app/home/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="w-full h-full">
+      <h1 className="text-white"></h1>
+    </div>
+  );
+}
+
+export default HomePage;
