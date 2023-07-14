d93255d904b649273bbcc55f18cf415c7cc2544b
Bon Aguilan <aguilanbon@gmail.com>
2023-07-14 16:11:02 +0800

    global search component

--- components/GlobalSearch.tsx
+++ components/GlobalSearch.tsx
@@ -8,8 +8,9 @@ function GlobalSearch() {
     <div className="flex justify-center items-center py-4 w-full">
       <Input
         placeholder="Search for something..."
+        value={""}
         onChange={(event) => console.log(event.target.value)}
-        className="w-96 text-white"
+        className="lg:w-[600px] sm:w-96 w-full text-white"
       />
     </div>
   );
