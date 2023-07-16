dac16afe31b9c958cda0b9251ec125d34e4cae9f
Bon Aguilan <aguilanbon@gmail.com>
2023-07-16 16:03:34 +0800

    ssr call on homepage load, get profile

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,11 +1,27 @@
 import NavigationBar from "@/components/NavigationBar";
+import { USER_PROFILE_URL } from "@/lib/constants/api_constants";
+import { User } from "@/lib/types/user.types";
+import { cookies } from "next/dist/client/components/headers";
 import React from "react";
 
-function HomePage() {
+async function getUserProfile() {
+  const nextCookie = cookies();
+  const jwtCookie = nextCookie.get("jwt");
+  const res = await fetch(USER_PROFILE_URL, {
+    headers: {
+      Cookie: `jwt=${jwtCookie?.value}`,
+    },
+  });
+  const data = await res.json();
+  return data.data;
+}
+
+async function HomePage() {
+  const data: User = await getUserProfile();
   return (
     <div className="w-full min-h-screen flex flex-col">
       <div className="w-full fixed">
-        <NavigationBar />
+        <NavigationBar userDetails={data} />
       </div>
       <div className="w-full mt-16 p-4"></div>
     </div>
