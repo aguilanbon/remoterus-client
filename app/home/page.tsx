dabba9f60c221914a944f5a7be29cfc1ef990a1b
Bon Aguilan <aguilanbon@gmail.com>
2023-07-19 22:24:23 +0800

    added back get user profile on profile page load

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,33 +2,33 @@ import NavigationBar from "@/components/NavigationBar";
 import { USER_PROFILE_URL } from "@/lib/constants/api_constants";
 import { User } from "@/lib/types/user.types";
 import { cookies } from "next/dist/client/components/headers";
+import { useRouter } from "next/navigation";
 import React from "react";
 
-// async function getUserProfile() {
-//   const nextCookie = cookies();
-//   const jwtCookie = nextCookie.get("jwt");
-//   const res = await fetch(USER_PROFILE_URL, {
-//     headers: {
-//       Cookie: `jwt=${jwtCookie?.value}`,
-//     },
-//   });
-//   const data = await res.json();
-//   const userData: User = {
-//     ...data.data,
-//     accessToken: jwtCookie?.value,
-//   };
+async function getUserProfile() {
+  const nextCookie = cookies();
+  const jwtCookie = nextCookie.get("jwt");
+  const res = await fetch(USER_PROFILE_URL, {
+    headers: {
+      Cookie: `jwt=${jwtCookie?.value}`,
+    },
+    cache: "no-store",
+  });
+  const data = await res.json();
+  const userData: User = {
+    ...data.data,
+    accessToken: jwtCookie?.value,
+  };
 
-//   return userData;
-// }
+  return userData;
+}
 
 async function HomePage() {
-  // const data: User = await getUserProfile();
+  const data: User = await getUserProfile();
   return (
     <div className="w-full min-h-screen flex flex-col">
       <div className="w-full fixed">
-        <NavigationBar
-        // userDetails={data}
-        />
+        <NavigationBar userDetails={data} />
       </div>
       <div className="w-full mt-16 p-4"></div>
     </div>
