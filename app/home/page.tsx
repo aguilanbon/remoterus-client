72154048d0931141d6a53f5711ebf2a83fa222c7
Bon Aguilan <aguilanbon@gmail.com>
2023-07-24 17:16:32 +0800

    remove uneccessary profile call on user info on home and profile

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,36 +1,32 @@
 import NavigationBar from "@/components/NavigationBar";
-import { USER_PROFILE_URL } from "@/lib/constants/api_constants";
-import { User } from "@/lib/types/user.types";
-import { useAtom } from "jotai";
-import { cookies } from "next/dist/client/components/headers";
 import React from "react";
 
-async function getUserProfile() {
-  const nextCookie = cookies();
-  const jwtCookie = nextCookie.get("jwt");
-  const res = await fetch(USER_PROFILE_URL, {
-    headers: {
-      Cookie: `jwt=${jwtCookie?.value}`,
-    },
-    cache: "no-store",
-  });
-  const data = await res.json();
-  const userData: User = {
-    ...data.data,
-  };
-  if (res.ok) {
-    return userData;
-  } else {
-    throw new Error("Auth is required to access this page.");
-  }
-}
+// async function getUserProfile() {
+//   const nextCookie = cookies();
+//   const jwtCookie = nextCookie.get("jwt");
+//   const res = await fetch(USER_PROFILE_URL, {
+//     headers: {
+//       Cookie: `jwt=${jwtCookie?.value}`,
+//     },
+//     cache: "no-store",
+//   });
+//   const data = await res.json();
+//   const userData: User = {
+//     ...data.data,
+//   };
+//   if (res.ok) {
+//     return userData;
+//   } else {
+//     throw new Error("Auth is required to access this page.");
+//   }
+// }
 
 async function HomePage() {
-  const data: User = await getUserProfile();
+  // const data: User = await getUserProfile();
   return (
     <div className="w-full min-h-screen flex flex-col">
       <div className="w-full fixed">
-        <NavigationBar userDetails={data} />
+        <NavigationBar />
       </div>
       <div className="w-full mt-16 p-4"></div>
     </div>
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,60 +1,44 @@
 import NavigationBar from "@/components/NavigationBar";
-import { USER_PROFILE_URL } from "@/lib/constants/api_constants";
-import { User, fullName } from "@/lib/types/user.types";
-import { cookies } from "next/dist/client/components/headers";
 import React from "react";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardHeader } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
-import {
-  ActivityLogIcon,
-  BackpackIcon,
-  GearIcon,
-  PersonIcon,
-} from "@radix-ui/react-icons";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tabs, TabsContent } from "@/components/ui/tabs";
 import AccountDetails from "@/components/profile/account_details";
+import AvatarCard from "@/components/profile/avatar_card";
 
-async function getUserProfile() {
-  const nextCookie = cookies();
-  const jwtCookie = nextCookie.get("jwt");
-  const res = await fetch(USER_PROFILE_URL, {
-    headers: {
-      Cookie: `jwt=${jwtCookie?.value}`,
-    },
-    cache: "no-store",
-  });
-  const data = await res.json();
-  const userData: User = {
-    ...data.data,
-    accessToken: jwtCookie?.value,
-  };
-  if (res.ok) {
-    return userData;
-  } else {
-    throw new Error("Auth is required to access this page.");
-  }
-}
+// async function getUserProfile() {
+//   const nextCookie = cookies();
+//   const jwtCookie = nextCookie.get("jwt");
+//   const res = await fetch(USER_PROFILE_URL, {
+//     headers: {
+//       Cookie: `jwt=${jwtCookie?.value}`,
+//     },
+//     cache: "no-store",
+//   });
+//   const data = await res.json();
+//   const userData: User = {
+//     ...data.data,
+//     accessToken: jwtCookie?.value,
+//   };
+//   if (res.ok) {
+//     return userData;
+//   } else {
+//     throw new Error("Auth is required to access this page.");
+//   }
+// }
 
 async function ProfilePage() {
-  const data: User = await getUserProfile();
-  const name: fullName = {
-    name:
-      data?.personalInformation?.name.first +
-      " " +
-      data?.personalInformation?.name.last,
-  };
+  // const data: User = await getUserProfile();
+  // const name: fullName = {
+  //   name:
+  //     data?.personalInformation?.name.first +
+  //     " " +
+  //     data?.personalInformation?.name.last,
+  // };
   return (
     <div className="w-full min-h-screen flex flex-col items-center">
       <div className="w-full fixed">
-        <NavigationBar userDetails={data} />
+        <NavigationBar />
       </div>
       <div className="w-3/4 mt-16 p-4 flex">
         <Tabs defaultValue="feed" className="w-full flex">
@@ -69,41 +53,7 @@ async function ProfilePage() {
                   <AvatarFallback>CN</AvatarFallback>
                 </Avatar>
               </CardHeader>
-              <CardContent className="w-full flex items-center justify-center flex-col">
-                <h1 className="text-lg font-semibold">{name.name}</h1>
-                <CardDescription className="mb-4">Position</CardDescription>
-                <TabsList className="w-full h-full flex flex-col space-y-4 bg-primary">
-                  <TabsTrigger
-                    value="feed"
-                    className="w-full flex items-center justify-start text-md"
-                  >
-                    <ActivityLogIcon className="mr-2" />
-                    Feed
-                  </TabsTrigger>
-                  <TabsTrigger
-                    value="jobs"
-                    className="w-full flex items-center justify-start text-md"
-                  >
-                    <BackpackIcon className="mr-2" />
-                    Jobs
-                  </TabsTrigger>
-                  <TabsTrigger
-                    value="account"
-                    className="w-full flex items-center justify-start text-md"
-                  >
-                    <PersonIcon className="mr-2" />
-                    Account Details
-                  </TabsTrigger>
-                  <TabsTrigger
-                    value="settings"
-                    className="w-full flex items-center justify-start text-md"
-                  >
-                    <GearIcon className="mr-2" />
-                    Settings
-                  </TabsTrigger>
-                </TabsList>
-                <CardContent></CardContent>
-              </CardContent>
+              <AvatarCard />
             </Card>
           </section>
           <section className="w-full flex items-center justify-start flex-col">
@@ -111,7 +61,7 @@ async function ProfilePage() {
               <TabsContent value="feed">feed</TabsContent>
               <TabsContent value="jobs">jobs</TabsContent>
               <TabsContent value="account">
-                <AccountDetails user={data} />
+                <AccountDetails />
               </TabsContent>
               <TabsContent value="settings">settings</TabsContent>
             </Card>
